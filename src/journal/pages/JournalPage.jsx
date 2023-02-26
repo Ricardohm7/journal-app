20ab4deef1bd89fc5ca89a022dee3099f2445659
Ricardo Hernandez <ricardo.hernandez@andonix.com>
2023-02-25 19:30:51 -0600

    JournalPage and NothingSelectedView developing complete

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,5 @@
+import { AddOutlined } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
@@ -6,8 +8,20 @@ export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography variant="h1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat tempore possimus debitis in suscipit. Tenetur, impedit. Voluptatem magnam corrupti tempora similique itaque amet nulla eaque debitis! Aliquid vitae suscipit architecto?</Typography> */}
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ':hover': { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}>
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   )
 }
--- src/journal/views/NothingSelectedView.jsx
+++ src/journal/views/NothingSelectedView.jsx
@@ -1,4 +1,5 @@
-import { Grid } from "@mui/material"
+import { NoteAltOutlined } from "@mui/icons-material"
+import { Grid, Typography } from "@mui/material"
 
 export const NothingSelectedView = () => {
   return (
@@ -8,9 +9,12 @@ export const NothingSelectedView = () => {
       direction="column"
       alignItems="center"
       justifyContent="center"
-      sx={{ minHeight: 'calc(100vh - 110px)', backgroundColor: 'primary.main' }}>
+      sx={{ minHeight: 'calc(100vh - 110px)', backgroundColor: 'primary.main', borderRadius: 5 }}>
       <Grid item xs={12}>
-
+        <NoteAltOutlined sx={{ fontSize: 100, color: "white" }} />
+      </Grid>
+      <Grid item xs={12}>
+        <Typography color="white" variant="h5">Start creating a note!</Typography>
       </Grid>
     </Grid>
   )

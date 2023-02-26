be08c499209bc02ce67d277e7561ce9e6e443201
Ricardo Hernandez <ricardo.hernandez@andonix.com>
2023-02-25 19:03:50 -0600

    Develop NoteVew component

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,13 @@
 import Typography from "@mui/material/Typography";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NoteView, NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography variant="h1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat tempore possimus debitis in suscipit. Tenetur, impedit. Voluptatem magnam corrupti tempora similique itaque amet nulla eaque debitis! Aliquid vitae suscipit architecto?</Typography>
+      {/* <Typography variant="h1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat tempore possimus debitis in suscipit. Tenetur, impedit. Voluptatem magnam corrupti tempora similique itaque amet nulla eaque debitis! Aliquid vitae suscipit architecto?</Typography> */}
+      {/* <NothingSelectedView /> */}
+      <NoteView />
     </JournalLayout>
   )
 }
---
+++ src/journal/views/NothingSelectedView.jsx
@@ -0,0 +1,17 @@
+import { Grid } from "@mui/material"
+
+export const NothingSelectedView = () => {
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: 'calc(100vh - 110px)', backgroundColor: 'primary.main' }}>
+      <Grid item xs={12}>
+
+      </Grid>
+    </Grid>
+  )
+}
